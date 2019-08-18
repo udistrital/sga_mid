33f69592b7f3457bdfc19e32246ed2185ac384f6
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-08-18 14:38:56 -0500

    #1 main configuration

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	_ "github.com/udistrital/sga_mid/routers"
+	notificacionlib "github.com/udistrital/notificaciones_lib"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
@@ -11,5 +15,26 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	logPath := "{\"filename\":\""
+	logPath += beego.AppConfig.String("logPath")
+	logPath += "\"}"
+	logs.SetLogger(logs.AdapterFile, logPath)
+
+	notificacionlib.InitMiddleware()
+	apistatus.Init()
 	beego.Run()
 }
