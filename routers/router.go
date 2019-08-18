829373936edaa552e61654b9db85d2cce1265b71
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-08-18 14:35:50 -0500

    #1 deleting automatic files

--- routers/router.go
+++ routers/router.go
@@ -15,16 +15,7 @@ import (
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
+
 	)
 	beego.AddNamespace(ns)
 }
