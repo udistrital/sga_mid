d69e56c78d1f2ada4f4cc88831b921d3f0f0e57c
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-08-29 16:53:41 -0500

    udistrital/evaluacion_inscripcion_crud#5 consultando datos de la inscripcion

--- controllers/archivo_icfes.go
+++ controllers/archivo_icfes.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/sga_mid/models"
-	// "github.com/udistrital/utils_oas/request"
+	"github.com/udistrital/utils_oas/request"
 )
 
 // ArchivoIcfesController ...
@@ -34,7 +34,9 @@ func (c *ArchivoIcfesController) PostArchivoIcfes() {
 	ArchivoIcfes := "Archivo procesado"
 	var alerta models.Alert
 	alertas := append([]interface{}{"Response:"})
+	periodo_id := "1"
 	fmt.Println("name",c.GetString("name"))
+	fmt.Println("periodo",periodo_id)
 	multipartFile, _, err := c.GetFile("archivo_icfes") 
 	if (err != nil) {
 		fmt.Println("err reading multipartFile", err)
@@ -53,10 +55,26 @@ func (c *ArchivoIcfesController) PostArchivoIcfes() {
 	}
 	lines := strings.Split(strings.Replace(string(file), "\r\n", "\n", -1), "\n")
 	for _, line := range lines {
+		// 0 código ICFEs del estudianate
 		// 1 para nombre del estudiante
 		recordFields := strings.Split(line,",")
 		if (len(recordFields) > 1) {
-			fmt.Println("line", recordFields[1])
+			aspirante_codigo_icfes := recordFields[0]
+			aspirante_nombre := recordFields[1]
+			fmt.Println("line", aspirante_codigo_icfes, aspirante_nombre)
+			// traer data de la inscripcion o inscripciones
+			fmt.Println("url","http://"+beego.AppConfig.String("InscripcionService")+"inscripcion_pregrado?limit=0&query=InscripcionId__Activo:true,InscripcionId__EstadoInscripcionId__Id:1,InscripcionId__PeriodoId:"+periodo_id+",CodigoIcfes:"+aspirante_codigo_icfes)
+			var inscripcionesRes []map[string]interface{}
+			errInscripciones := request.GetJson("http://"+beego.AppConfig.String("InscripcionService")+"inscripcion_pregrado?limit=0&query=InscripcionId__Activo:true,InscripcionId__EstadoInscripcionId__Id:1,InscripcionId__PeriodoId:"+periodo_id+",CodigoIcfes:"+aspirante_codigo_icfes, &inscripcionesRes)
+			if errInscripciones != nil {
+				alertas = append(alertas, errInscripciones)
+				alerta.Body = alertas
+				alerta.Type = "error"
+				alerta.Code = "400"
+				c.ServeJSON()
+			} else {
+				fmt.Println("inscripcion", len(inscripcionesRes), inscripcionesRes)
+			}	
 		}
 	} 
 
