5060b8b5c6c5995769fccbf1ce8fad28b0fbf117
Ricardo Avendaño Casas <ricardoavendanocasas@MacBook-Pro-de-Ricardo.local>
2020-11-14 17:11:22 -0500

    fix: Ajustar funcionalidad de la tabla de consulta

--- controllers/consulta_calendario_academico.go
+++ controllers/consulta_calendario_academico.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 
 	"github.com/astaxie/beego"
+<<<<<<< HEAD
 	"github.com/astaxie/beego/logs"
+=======
+	"github.com/udistrital/sga_mid/models"
+>>>>>>> 292d6f0... fix: Ajustar funcionalidad de la tabla de consulta
 	"github.com/udistrital/utils_oas/request"
 )
 
@@ -77,4 +81,102 @@ func (c *ConsultaCalendarioAcademicoController) GetAll() {
 // @Failure 403 :id is empty
 // @router /:id [get]
 func (c *ConsultaCalendarioAcademicoController) GetOnePorId() {
+
+	var resultados []map[string]interface{}
+	var resultado map[string]interface{}
+	var consultaCalendario map[string]interface{}
+	var consultaCalendarioResultado []map[string]interface{}
+	var procesoCalendario map[string]interface{}
+	var procesoCalendarioResultado []map[string]interface{}
+	var alerta models.Alert
+	alertas := append([]interface{}{"Response:"})
+	idStr := c.Ctx.Input.Param(":id")
+
+	if resultado["Type"] != "error" {
+		var calendarios []map[string]interface{}
+
+		errcalendario := request.GetJson("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento?query=TipoEventoId__Id.CalendarioID__Id:"+idStr, &calendarios)
+
+		if calendarios != nil {
+
+			if errcalendario == nil {
+
+				for _, calendario := range calendarios {
+
+					var documentos map[string]interface{}
+					var calendarioID map[string]interface{}
+
+					calendarioID = calendario["TipoEventoId"].(map[string]interface{})["CalendarioID"].(map[string]interface{})
+
+					documentoID := fmt.Sprintf("%.f", calendarioID["DocumentoId"].(float64))
+
+					errcdocumento := request.GetJson("http://"+beego.AppConfig.String("DocumentosService")+"/documento/"+documentoID, &documentos)
+
+					if errcdocumento == nil {
+						consultaCalendario = map[string]interface{}{
+							"Nombre":    calendario["Nombre"].(string),
+							"Enlace":    documentos["Enlace"].(string),
+							"Metadatos": documentos["Metadatos"].(string),
+						}
+					}
+					var responsableString string
+					for _, calendario2 := range calendarios {
+
+						calendarioResponsableID := fmt.Sprintf("%.f", calendario2["Id"].(float64))
+						var responsables []map[string]interface{}
+						errresponsable := request.GetJson("http://localhost:8013/v1/calendario_evento_tipo_publico?query=CalendarioEventoId__Id:"+calendarioResponsableID, &responsables)
+
+						if errresponsable == nil {
+							var responsablesID map[string]interface{}
+							responsablesID = responsables[0]["TipoPublicoId"].(map[string]interface{})
+							responsableID := fmt.Sprintf(responsablesID["Nombre"].(string))
+
+							responsableString = responsableID + ", " + responsableString
+						}
+					}
+
+					consultaCalendarioResultado = append(consultaCalendarioResultado, consultaCalendario)
+					responsableString = responsableString[:len(responsableString)-2]
+					procesoCalendario = map[string]interface{}{
+						"Nombre":      calendario["Nombre"].(string),
+						"FechaInicio": calendario["FechaInicio"].(string),
+						"FechaFin":    calendario["FechaFin"].(string),
+						"Responsable": responsableString,
+					}
+
+					procesoCalendarioResultado = append(procesoCalendarioResultado, procesoCalendario)
+				}
+
+				resultado = map[string]interface{}{
+					"Nombre":          calendarios[0]["TipoEventoId"].(map[string]interface{})["CalendarioID"].(map[string]interface{})["Nombre"].(string),
+					"listaCalendario": consultaCalendarioResultado,
+					"proceso":         procesoCalendarioResultado,
+				}
+				resultados = append(resultados, resultado)
+
+				c.Data["json"] = resultados
+
+			} else {
+				alertas = append(alertas, errcalendario.Error())
+				alerta.Code = "400"
+				alerta.Type = "error"
+				alerta.Body = alertas
+				c.Data["json"] = alerta
+			}
+
+		} else {
+			c.Data["json"] = calendarios
+		}
+	} else {
+		if resultado["Body"] == "<QuerySeter> no row found" {
+			c.Data["json"] = nil
+		} else {
+			alertas = append(alertas, resultado["Body"])
+			alerta.Code = "400"
+			alerta.Type = "error"
+			alerta.Body = alertas
+			c.Data["json"] = alerta
+		}
+	}
+	c.ServeJSON()
 }
