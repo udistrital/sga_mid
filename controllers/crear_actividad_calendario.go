e290a53ee8fc7e88c7d3efb3dce8a12d2c3cda0b
gabrielablanco <ferblanco1003@gmail.com>
2020-11-25 22:06:13 -0500

    Se logro identificar una variable redundante la cual no almacenaba nada y por ende no agregaba informacion a la tabla calendario_evento_tipo_publico, se añade la excepcion de borrar una actividad si no se crea la tabla de rompimiento

--- controllers/crear_actividad_calendario.go
+++ controllers/crear_actividad_calendario.go
@@ -21,93 +21,89 @@ func (c *ActividadCalendarioController) URLMapping() {
 // @Title PostActividadCalendario
 // @Description Agregar actividad calendario, tipo_publico y tabla de rompimiento calendario_evento_tipo_publico
 // @Param	body		body 	{}	true		"body Agregar Actividad calendario content"
-// @Success 201 {int}
-// @Failure 400 the request contains incorrect syntax
+// @Success 200 {}
+// @Failure 403 body is empty
 // @router / [post]
 func (c *ActividadCalendarioController) PostActividadCalendario() {
 
-	//Almacena el resultado del json en algunas operaciones
-	var resActividad map[string]interface{}
 	//Almacena el json que se trae desde el cliente
 	var actividadCalendario map[string]interface{}
+	//Almacena el resultado del json en algunas operaciones
 	var actividadCalendarioPost map[string]interface{}
 	var IdActividad interface{}
+	var actividadPersonaPost map[string]interface{}
 
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &actividadCalendario); err == nil {
-		//Guarda el JSON de que recibe de la actividad
-		actividad := actividadCalendario["Actividad"]
-		errActividad := request.SendJson("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento", "POST", &resActividad, actividad)
-		if errActividad == nil && fmt.Sprintf("%v", resActividad["System"]) != "map[]" && resActividad["actividadId"] != nil {
-			if resActividad["Status"] != 400 {
-				IdActividad = resActividad["Id"]
+		//Solicitid post a eventos service enviando el json recibido
+		errActividad := request.SendJson("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento", "POST", &actividadCalendarioPost, actividadCalendario)
+		if errActividad == nil && fmt.Sprintf("%v", actividadCalendarioPost["System"]) != "map[]" && actividadCalendarioPost["actividadId"] != nil {
+			if actividadCalendarioPost["Status"] != 403 {
+				IdActividad = actividadCalendarioPost["actividadId"]
 				resultado := map[string]interface{}{
+					"Id":           actividadCalendarioPost["actividadId"],
 					"Nombre":       actividadCalendarioPost["Nombre"],
 					"Descripcion":  actividadCalendarioPost["Descripcion"],
 					"FechaInicio":  actividadCalendarioPost["FechaInicio"],
 					"FechaFin":     actividadCalendarioPost["FechaFin"],
 					"Activo":       true,
-					"responsable":  actividadCalendarioPost["responsable"],
 					"TipoEventoId": actividadCalendarioPost["TipoEventoId"],
+					//"TipoEventoId": map[string]interface{}{"Id": 0},
 				}
-				resultado["DocumentoId"] = resActividad["DocumentoId"]
+				fmt.Println(IdActividad)
+				resultado["DocumentoId"] = actividadCalendarioPost["DocumentoId"]
 				c.Data["json"] = resultado
 
 			} else {
 				logs.Error(errActividad)
-				c.Data["system"] = resActividad
-				c.Abort("400")
+				c.Data["json"] = map[string]interface{}{"Code": "403", "Body": errActividad.Error(), "Type": "error"}
+				c.Data["system"] = actividadCalendarioPost
+				c.Abort("403")
 			}
 		} else {
 			logs.Error(errActividad)
-			c.Data["system"] = resActividad
-			c.Abort("400")
+			c.Data["json"] = map[string]interface{}{"Code": "403", "Body": errActividad.Error(), "Type": "error"}
+			c.Data["system"] = actividadCalendarioPost
+			c.Abort("403")
 		}
+
 		var totalPublico []map[string]interface{}
+
 		//Guarda el JSON de la tabla publico
-		totalPublico = actividadCalendario["Publico"].([]map[string]interface{})
-		fmt.Println(actividad)
+		totalPublico = actividadCalendario["responsable"].([]map[string]interface{})
 
 		for _, publicoTemp := range totalPublico {
 			CalendarioEventoTipoPersona := map[string]interface{}{
-
 				"Activo":             true,
-				"TipoPublicoId":      publicoTemp["Publico"].(map[string]interface{})["IdPublico"].(float64),
+				"responsable":        publicoTemp["responsable"].(map[string]interface{})["IdPublico"].(float64),
 				"CalendarioEventoId": IdActividad.(float64),
 			}
 
-			fmt.Println(CalendarioEventoTipoPersona)
-			var resActividadPersona map[string]interface{}
-			var actividadPersonaPost map[string]interface{}
+			errActividadPersona := request.SendJson("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento_tipo_publico", "POST", &actividadPersonaPost, CalendarioEventoTipoPersona)
 
-			errActividadPersona := request.SendJson("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento_tipo_publico", "POST", &resActividadPersona, CalendarioEventoTipoPersona)
-			if errActividadPersona == nil && fmt.Sprintf("%v", resActividadPersona["System"]) != "map[]" && resActividadPersona["Id"] != nil {
-				if resActividadPersona["Status"] != 400 {
-					//IdActividadPersona := resActividadPersona["Id"] //.(map[string]interface{})[""]
+			if errActividadPersona == nil && fmt.Sprintf("%v", actividadPersonaPost["System"]) != "map[]" && actividadPersonaPost["Id"] != nil {
+				if actividadPersonaPost["Status"] != 403 {
 					resultado := map[string]interface{}{
 						"Activo":             true,
 						"TipoPublicoId":      actividadPersonaPost["TipoPublicoId"],
 						"CalendarioEventoId": actividadPersonaPost["CalendarioEventoId"],
 					}
-					resultado["DocumentoId"] = resActividadPersona["DocumentoId"]
+					resultado["DocumentoId"] = actividadPersonaPost["DocumentoId"]
 					c.Data["json"] = resultado
 
 				} else {
 					var resultado2 map[string]interface{}
-					request.SendJson(fmt.Sprintf("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento/%.f", actividadCalendarioPost["Id"]), "DELETE", &resultado2, nil)
+					request.SendJson(fmt.Sprintf("http://"+beego.AppConfig.String("EventoService")+"/calendario_evento/%.f", actividadCalendarioPost["actividadId"]), "DELETE", &resultado2, nil)
 					logs.Error(errActividadPersona)
-					c.Data["system"] = resActividadPersona
+					c.Data["system"] = actividadPersonaPost
 					c.Abort("400")
+
 				}
 			} else {
 				logs.Error(errActividadPersona)
-				c.Data["system"] = resActividadPersona
+				c.Data["system"] = actividadPersonaPost
 				c.Abort("400")
 			}
 		}
-
-	} else {
-		logs.Error(err)
-		c.Data["system"] = err
-		c.Abort("400")
 	}
+	c.ServeJSON()
 }
