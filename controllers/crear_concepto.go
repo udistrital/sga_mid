6db45b951ed3edadecd0071371211d5d10932a04
gabrielablanco <ferblanco1003@gmail.com>
2020-11-28 20:29:07 -0500

    Se añaden nuevos campos para agregar un parametro_periodo en este caso un factor, se corrige la estructura de almacenamiento del json y se hacen las respectivas pruebas

--- controllers/crear_concepto.go
+++ controllers/crear_concepto.go
@@ -27,41 +27,56 @@ func (c *ConceptoController) URLMapping() {
 func (c *ConceptoController) PostConcepto() {
 
 	var ConceptoFactor map[string]interface{}
+	var AuxConceptoPost map[string]interface{}
 	var ConceptoPost map[string]interface{}
 	var IdConcepto interface{}
+	var NumFactor interface{}
+	var Vigencia interface{}
 
 	//Se guarda el json que se pasa por parametro
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &ConceptoFactor); err == nil {
 		Concepto := ConceptoFactor["Concepto"]
-		errConcepto := request.SendJson("http://"+beego.AppConfig.String("ParametroService")+"parametro", "POST", &ConceptoPost, Concepto)
+		IdConcepto = Concepto.(map[string]interface{})["Id"]
+		errConcepto := request.SendJson("http://"+beego.AppConfig.String("ParametroService")+"parametro", "POST", &AuxConceptoPost, Concepto)
+		ConceptoPost = AuxConceptoPost["Data"].(map[string]interface{})
 		if errConcepto == nil && fmt.Sprintf("%v", ConceptoPost["System"]) != "map[]" && ConceptoPost["Id"] != nil {
 			if ConceptoPost["Status"] != 400 {
-				IdConcepto = ConceptoPost["Id"]
 				c.Data["json"] = ConceptoPost
 			} else {
 				logs.Error(errConcepto)
 				c.Data["json"] = map[string]interface{}{"Code": "400", "Body": errConcepto.Error(), "Type": "error"}
 				c.Data["system"] = ConceptoPost
-				c.Abort("400")
 			}
 		} else {
 			logs.Error(errConcepto)
-			c.Data["json"] = map[string]interface{}{"Code": "400", "Body": errConcepto.Error(), "Type": "error"}
 			c.Data["system"] = ConceptoPost
-			c.Abort("400")
 		}
 
-		NumFactor := ConceptoFactor["Factor"] //Valor que trae el numero del factor
+		fmt.Println("Punto de control 2")
+		Vigencia = ConceptoFactor["Vigencia"] //Valor del id de la vigencia (periodo)
+		NumFactor = ConceptoFactor["Factor"]  //Valor que trae el numero del factor y el salario minimo
+
+		fmt.Println(Vigencia)
+		fmt.Println(NumFactor)
+
+		ValorFactor := fmt.Sprintf("%v", NumFactor.(map[string]interface{})["Valor"].(map[string]interface{})["NumFactor"])
+		SalarioMin := fmt.Sprintf("%v", NumFactor.(map[string]interface{})["Valor"].(map[string]interface{})["SalarioMinimo"])
+		Valor := "{\n    \"NumFactor\": " + ValorFactor + ", \n \"SalarioMinimo\": " + SalarioMin + "\n  }"
+
 		Factor := map[string]interface{}{
-			"ParametroId": map[string]interface{}{"Id": IdConcepto.(float64)},
-			"Valor":       map[string]interface{}{"Factor": NumFactor.(map[string]interface{})["Factor"].(float64)},
-			"Activo":      true,
+			"ParametroId":       map[string]interface{}{"Id": IdConcepto.(float64)},
+			"PeriodoId":         map[string]interface{}{"Id": Vigencia.(map[string]interface{})["Id"].(float64)},
+			"Valor":             Valor,
+			"FechaCreacion":     Concepto.(map[string]interface{})["FechaCreacion"],
+			"FechaModificacion": Concepto.(map[string]interface{})["FechaCreacion"],
+			"Activo":            true,
 		}
 
+		var AuxFactor map[string]interface{}
 		var FactorPost map[string]interface{}
 
-		errFactor := request.SendJson("http://"+beego.AppConfig.String("ParametroService")+"parametro_periodo", "POST", &FactorPost, Factor)
-
+		errFactor := request.SendJson("http://"+beego.AppConfig.String("ParametroService")+"parametro_periodo", "POST", &AuxFactor, Factor)
+		FactorPost = AuxFactor["Data"].(map[string]interface{})
 		if errFactor == nil && fmt.Sprintf("%v", FactorPost["System"]) != "map[]" && FactorPost["Id"] != nil {
 			if FactorPost["Status"] != 400 {
 				c.Data["json"] = FactorPost
@@ -70,12 +85,12 @@ func (c *ConceptoController) PostConcepto() {
 				request.SendJson(fmt.Sprintf("http://"+beego.AppConfig.String("ParametroService")+"parametro/%.f", ConceptoPost["Id"]), "DELETE", &resultado2, nil)
 				logs.Error(errFactor)
 				c.Data["system"] = FactorPost
-				c.Abort("400")
+				c.Data["json"] = map[string]interface{}{"Code": "400", "Body": errFactor.Error(), "Type": "error"}
 			}
 		} else {
 			logs.Error(errFactor)
 			c.Data["system"] = FactorPost
-			c.Abort("400")
 		}
 	}
+	c.ServeJSON()
 }
