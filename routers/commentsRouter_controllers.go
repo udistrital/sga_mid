2d3ddb7ed5425726484c1a889c33150077686a22
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2021-10-20 23:48:50 -0500

    feat: ajuste de flujos para la creación de nuevos terceros en la producción académica. udistrital/sga_cliente#700

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -765,6 +765,15 @@ func init() {
 
     beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:PersonaController"],
         beego.ControllerComments{
+            Method: "GuardarAutor",
+            Router: "/guardar_autor",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:PersonaController"],
+        beego.ControllerComments{
             Method: "GuardarDatosComplementarios",
             Router: "/guardar_complementarios",
             AllowHTTPMethods: []string{"post"},
