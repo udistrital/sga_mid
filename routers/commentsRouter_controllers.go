137d741f6a87f79826a102f5dc0e43aa0e382e00
nefawaltpint <nefawaltpint@gmail.com>
2023-06-29 17:40:22 -0500

    feat: udistrital/sga_cliente#1332 creacion controlador reportes ptd
    - Creación de controlador para reporte plan trabajo docente en formato excel

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -1294,6 +1294,15 @@ func init() {
             Filters: nil,
             Params: nil})
 
+    beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:ReportesController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:ReportesController"],
+        beego.ControllerComments{
+            Method: "ReporteCargaLectiva",
+            Router: "/plan_trabajo_docente/:docente_id/:vinculacion_id/:periodo_id/:carga",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:SolicitudDocenteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_mid/controllers:SolicitudDocenteController"],
         beego.ControllerComments{
             Method: "PostSolicitudDocente",
